35a31271b70ed286fd9997a064f9f5d3ca969c9b
Lavv ⚝ <113632168+lavkush-maurya@users.noreply.github.com>
2023-07-05 12:24:15 +0000

    blury navbar

--- components/BuiltWith.tsx
+++ components/BuiltWith.tsx
@@ -7,19 +7,19 @@ export function BuiltWith() {
     <div className="flex items-center space-x-1">
       <span className="mr-1 text-gray-500 dark:text-gray-400">Built with</span>
       <div className="flex space-x-1.5">
-        <Link href="https://nextjs.org?ref=leohuynh.dev">
+        <Link href="https://nextjs.org?ref=https://github.com/lavkush-maurya/next-blog">
           <DevIcon type="NextJS" className="h-5 w-5" />
         </Link>
-        <Link href="https://tailwindcss.com?ref=leohuynh.dev">
+        <Link href="https://tailwindcss.com?ref=.https://github.com/lavkush-maurya/next-blog">
           <DevIcon type="TailwindCSS" className="h-5 w-5" />
         </Link>
-        <Link href="https://www.prisma.io?ref=leohuynh.dev">
+        <Link href="https://www.prisma.io?ref=.https://github.com/lavkush-maurya/next-blog">
           <DevIcon type="Prisma" className="h-5 w-5" />
         </Link>
-        <Link href="https://www.typescriptlang.org?ref=leohuynh.dev">
+        <Link href="https://www.typescriptlang.org?ref=.https://github.com/lavkush-maurya/next-blog">
           <DevIcon type="Typescript" className="h-5 w-5" />
         </Link>
-        <Link href="https://umami.is?ref=leohuynh.dev" className="pl-px">
+        <Link href="https://umami.is?ref=.https://github.com/lavkush-maurya/next-blog" className="pl-px">
           <DevIcon type="Umami" className="h-5 w-5" />
         </Link>
       </div>
