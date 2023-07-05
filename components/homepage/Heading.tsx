815e5f8fe0c5f62885043fa50f769bc8e6ee2a00
Lavv ⚝ <113632168+lavkush-maurya@users.noreply.github.com>
2023-07-05 11:25:37 +0000

    india-flag

--- components/homepage/Heading.tsx
+++ components/homepage/Heading.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable no-use-before-define */
 import { siteMetadata } from '~/data/siteMetadata'
-
+import Twemoji from '../Twemoji'
 export function Heading() {
   return (
     <h1 className="text-neutral-900 dark:text-neutral-200">
       I'm <span className="font-medium">{siteMetadata.fullName}</span> - a dedicated{' '}
       <span className="font-medium">Developer</span> in{' '}
       <span className="hidden font-medium">Surat,Gujarat</span>
-      <span className="absolute ml-1.5 inline-flex pt-[3px]">🇮🇳.</span>
+      <span className="absolute ml-1.5 inline-flex pt-[3px]"><Twemoji emoji='flag-india' /></span>
     </h1>
   )
 }
