4b5af372de4cf3b929b53a83ab02bd850701a303
Lavv ⚝ <113632168+lavkush-maurya@users.noreply.github.com>
2023-11-25 04:40:24 +0000

    generate sitemap update

--- scripts/generate-sitemap.ts
+++ scripts/generate-sitemap.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import { globby } from 'globby'
 import prettier from 'prettier'
 
-let SITE_URL = 'https://www.leohuynh.dev'
+let SITE_URL = 'https://lavv-blog.vercel.app/'
 
 ;(async () => {
   console.log('Generating sitemap...')
