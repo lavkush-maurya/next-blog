76e3bd37563107980341e4b2779cfffe24e5f559
Lavv ⚝ <113632168+lavkush-maurya@users.noreply.github.com>
2023-12-22 07:12:46 +0000

    added linktree

---
+++ pages/todo.tsx
@@ -0,0 +1,15 @@
+export default function Linktree() {
+  return (
+    <div className="flex flex-col items-center justify-center pt-4 md:pt-10 xl:pt-20">
+      <iframe
+        title="Linktree"
+        src="https://linktr.ee"
+        width="420"
+        height="800"
+        frameBorder="0"
+        scrolling="no"
+        style={{ overflow: 'hidden' }}
+      ></iframe>
+    </div>
+  )
+}
